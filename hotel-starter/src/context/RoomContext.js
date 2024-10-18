b63cd8dd6ad8fe87a39bcb88319d46224b1a2b3a
Gnana <shekark509@gmail.com>
2024-10-18 18:58:38 +0530

    updated work

--- hotel-starter/src/context/RoomContext.js
+++ hotel-starter/src/context/RoomContext.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {createContext, useEffect, useState} from 'react';
+//data//
+import {roomData} from '../data'
+//create context//
+export const RoomContext = createContext();
 
-const RoomContext = () => {
-  return <div>RoomContext</div>;
+const RouterProvider = ({children}) => {
+  console.log(roomData)
+  return (
+    <RoomContext.Provider value={'room context'}>
+      {children}
+    </RoomContext.Provider>
+  )
 };
 
-export default RoomContext;
+export default RouterProvider;
--- hotel-starter/src/pages/RoomDetails.js
+++ hotel-starter/src/pages/RoomDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const RoomDetails = () => {
   return <div>RoomDetails</div>;
 };
