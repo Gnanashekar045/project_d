692ec73557fafb5e0576c50345103d68c295b1ae
Gnana <shekark509@gmail.com>
2024-10-19 22:07:21 +0530

    updated

--- react-tailwindcss-setup/src/pages/Home/hero.jsx
+++ react-tailwindcss-setup/src/pages/Home/hero.jsx
@@ -16,21 +16,21 @@ const Hero = () => {
   const swiperData = [
     {
       id: 1,
-      bgImg: "https://source.unsplash.com/random/?fitness",
+      bgImg: "https://static.toiimg.com/thumb/imgsize-23456,msid-101689456,width-600,resizemode-4/101689456.jpg",
       slogan: "Keep your body",
       title: "Burning",
       desc: "Get ready to burn off some serious fat with our high quality products.",
     },
     {
       id: 2,
-      bgImg: "https://source.unsplash.com/random/?gym",
+      bgImg: "https://pbs.twimg.com/media/FqmzcsuaYAIQiDl.jpg:large",
       slogan: "Just Believe In",
       title: "Yourself",
       desc: "We are here to help you achieve your goals.",
     },
     {
       id: 3,
-      bgImg: "https://source.unsplash.com/random/?yoga",
+      bgImg: "https://files.prokerala.com/movies/pics/800/gangotri-fame-kavya-hd-photos-114041.jpg",
       slogan: "Grow Your Body",
       title: "Strength",
       desc: "Make your body stronger with our high quality products.",
@@ -53,11 +53,11 @@ const Hero = () => {
         >
           {/**/}
           {/*loop the carousle content here*/}
-          {/*Swiper Slids*/}
-          <SwiperSlide
+          {swiperData.map((data) => (
+            <SwiperSlide key={data.id}
             className="w-full h-[70vh] relative"
             style={{
-              backgroundImage: "url('https://i0.wp.com/afamuche.com/wp-content/uploads/2020/02/9-Things-To-Do-Before-9am-3.jpg?resize=1024%2C683&ssl=1')",
+              backgroundImage: `url(${data.bgImg})`,
               backgroundSize: "cover",
               backgroundPosition: "center",
               backgroundRepeat: "no-repeat",
@@ -68,17 +68,18 @@ const Hero = () => {
             {/**/}
             <div className="w-full h-full flex items-center justify-center flex-col z-10 lg:px-24 md:px-16 sm:px-6 px-4" >
               <h5 className="lg:text-4xl md:text-3xl sm:text-2xl text-2xl text-indigo-400 font-semibold mb-2 uppercase">
-                keep you body
+                {data.slogan}
               </h5>
               <h1 className="lg:text-8x1 md:text-7x1 sm:text-5xl text-5xl font-bold  text-white mb-4 uppercase">
-                Burning
+                {data.title}
               </h1>
               <p className="lg:text-lg md:text-base sm:text-base text-base text-gray-500 font-medium mb-6 text-center">
-                Get ready to burn off some serious with our high quality products.
+                {data.desc}
               </p>
               <PrimaryBtn className="lg:w-[15%] md:w-[35%] sm:w-[75] w-80 h-14 mt-5 text-xl font-semibold rounded-full">Get Started</PrimaryBtn>
             </div>
-          </SwiperSlide>
+          </SwiperSlide>))}
+          
         </Swiper>
       </div>
     </>
