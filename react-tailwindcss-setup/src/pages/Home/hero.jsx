54f17bb7cfa6495c3ce296af36865b150f89e074
Gnana <shekark509@gmail.com>
2024-10-19 20:50:29 +0530

    update

--- react-tailwindcss-setup/src/pages/Home/hero.jsx
+++ react-tailwindcss-setup/src/pages/Home/hero.jsx
@@ -1,68 +1,74 @@
 import React from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 // import required modules
-import { Autoplay, Navigation } from 'swiper/modules';
+import { Autoplay, Navigation } from "swiper/modules";
 
 const Hero = () => {
-    //dummy data for coreusel
-    const swiperData = [
-        {
-          id: 1,
-          bgImg: "https://source.unsplash.com/random/?fitness",
-          slogan: "Keep your body",
-          title: "Burning",
-          desc: "Get ready to burn off some serious fat with our high quality products.",
-        },
-        {
-          id: 2,
-          bgImg: "https://source.unsplash.com/random/?gym",
-          slogan: "Just Believe In",
-          title: "Yourself",
-          desc: "We are here to help you achieve your goals.",
-        },
-        {
-          id: 3,
-          bgImg: "https://source.unsplash.com/random/?yoga",
-          slogan: "Grow Your Body",
-          title: "Strength",
-          desc: "Make your body stronger with our high quality products.",
-        },
-      ];
+  //dummy data for coreusel
+  const swiperData = [
+    {
+      id: 1,
+      bgImg: "https://source.unsplash.com/random/?fitness",
+      slogan: "Keep your body",
+      title: "Burning",
+      desc: "Get ready to burn off some serious fat with our high quality products.",
+    },
+    {
+      id: 2,
+      bgImg: "https://source.unsplash.com/random/?gym",
+      slogan: "Just Believe In",
+      title: "Yourself",
+      desc: "We are here to help you achieve your goals.",
+    },
+    {
+      id: 3,
+      bgImg: "https://source.unsplash.com/random/?yoga",
+      slogan: "Grow Your Body",
+      title: "Strength",
+      desc: "Make your body stronger with our high quality products.",
+    },
+  ];
   return (
     <>
       <div className="w-full h-auto">
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          navigation={true}
+          modules={[Autoplay, Navigation]}
+          className="mySwiper w-full h-auto"
+        >
+          <SwiperSlide
+            className="w-full h-[70vh] relative"
+            style={{
+              backgroundImage: "url('https://i0.wp.com/afamuche.com/wp-content/uploads/2020/02/9-Things-To-Do-Before-9am-3.jpg?resize=1024%2C683&ssl=1')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            {/*this is the overlap box with some black bg color*/}
+            <div className="w-full h-full bg-black/80 absolute top-0 left-0 -z-10"></div>
+            {/**/}
+            <div className="w-full h-full flex items-center justify-center flex-col z-10 lg:px-24 md:px-16 sm:px-6 px-4" >
+              <h5 className="lg:text-4xl md:text-3xl sm:text-2xl text-2xl text-indigo-400 font-semibold mb-2 uppercase">
+                keep you body
+              </h5>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </>
   );
