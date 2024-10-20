6975f084461a081b41fc34d738632ed294d748af
Gnana <gnanashekar045@gmail.com>
2024-10-20 08:55:11 +0530

    div upated

--- react-tailwindcss-setup/src/pages/Home/hero.jsx
+++ react-tailwindcss-setup/src/pages/Home/hero.jsx
@@ -16,7 +16,8 @@ const Hero = () => {
   const swiperData = [
     {
       id: 1,
-      bgImg: "https://jamespatrick.com/wp-content/uploads/2023/12/Fitness-Ashley-Wilke.jpg",
+      bgImg:
+        "https://jamespatrick.com/wp-content/uploads/2023/12/Fitness-Ashley-Wilke.jpg",
       slogan: "Keep your body",
       title: "Burning",
       desc: "Get ready to burn off some serious fat with our high quality products.",
@@ -54,35 +55,52 @@ const Hero = () => {
           {/**/}
           {/*loop the carousle content here*/}
           {swiperData.map((data) => (
-            <SwiperSlide key={data.id}
-            className="w-full h-[70vh] relative"
-            style={{
-              backgroundImage: `url(${data.bgImg})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-            }}
-          >
-            {/*this is the overlap box with some black bg color*/}
-            <div className="w-full h-full bg-black/80 absolute top-0 left-0 -z-10"></div>
-            {/**/}
-            <div className="w-full h-full flex items-center justify-center flex-col z-10 lg:px-24 md:px-16 sm:px-6 px-4" >
-              <h5 className="lg:text-4xl md:text-3xl sm:text-2xl text-2xl text-indigo-400 font-semibold mb-2 uppercase">
-                {data.slogan}
-              </h5>
-              <h1 className="lg:text-8x1 md:text-7x1 sm:text-5xl text-5xl font-bold  text-white mb-4 uppercase">
-                {data.title}
-              </h1>
-              <p className="lg:text-lg md:text-base sm:text-base text-base text-gray-500 font-medium mb-6 text-center">
-                {data.desc}
-              </p>
-              <PrimaryBtn className="lg:w-[15%] md:w-[35%] sm:w-[75] w-80 h-14 mt-5 text-xl font-semibold rounded-full">Get Started</PrimaryBtn>
-            </div>
-          </SwiperSlide>))}
+            <SwiperSlide
+              key={data.id}
+              className="w-full h-[70vh] relative"
+              style={{
+                backgroundImage: `url(${data.bgImg})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }}
+            >
+              {/*this is the overlap box with some black bg color*/}
+              <div className="w-full h-full bg-black/80 absolute top-0 left-0 -z-10"></div>
+              {/**/}
+              <div className="w-full h-full flex items-center justify-center flex-col z-10 lg:px-24 md:px-16 sm:px-6 px-4">
+                <h5 className="lg:text-4xl md:text-3xl sm:text-2xl text-2xl text-indigo-400 font-semibold mb-2 uppercase">
+                  {data.slogan}
+                </h5>
+                <h1 className="lg:text-8x1 md:text-7x1 sm:text-5xl text-5xl font-bold  text-white mb-4 uppercase">
+                  {data.title}
+                </h1>
+                <p className="lg:text-lg md:text-base sm:text-base text-base text-gray-500 font-medium mb-6 text-center">
+                  {data.desc}
+                </p>
+                <PrimaryBtn className="lg:w-[15%] md:w-[35%] sm:w-[75%] w-80 h-14 mt-5 text-xl font-semibold rounded-full">
+                  Get Started
+                </PrimaryBtn>
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
         {/*Bnner type card*/}
-        <div className="w-full h-auto flex itmes-center lg:justify-between md:justify-between sm:justify-center justify-center lg:gap-7 md:gap-7 sm:gap-5 gap-5 lg:px-24 md:px-16 sm:px-6 px-6 mt-6 flex-wrap">
-          
+        <div className="w-full h-auto flex items-center lg:justify-between md:justify-between sm:justify-center justify-center lg:gap-7 md:gap-7 sm:gap-5 gap-5 lg:px-24 md:px-16 sm:px-6 px-6 mt-6 flex-wrap">
+          <div className="flex items-center justify-center flex-col lg:w-fit md:w-[48% sm:w-[48%] w-full lg:py-6 md:py-7 sm:py-8 py-9 px-10 bg-black/10 hover:bg-black/30 rounded-md ease-out duration-700 cursor-pointer">
+          <h5 className="h5 text-4X1 text-indigo-400 font-semibold mb-2 uppercase">
+            1200+
+          </h5>
+          <p className="text-lg text-gray-600 font-medium">Members</p>
+          </div>
+        </div>
+        <div className="w-full h-auto flex items-center lg:justify-between md:justify-between sm:justify-center justify-center lg:gap-7 md:gap-7 sm:gap-5 gap-5 lg:px-24 md:px-16 sm:px-6 px-6 mt-6 flex-wrap">
+          <div className="flex items-center justify-center flex-col lg:w-fit md:w-[48% sm:w-[48%] w-full lg:py-6 md:py-7 sm:py-8 py-9 px-10 bg-black/10 hover:bg-black/30 rounded-md ease-out duration-700 cursor-pointer">
+          <h5 className="h5 text-4X1 text-indigo-400 font-semibold mb-2 uppercase">
+            1200+
+          </h5>
+          <p className="text-lg text-gray-600 font-medium">Members</p>
+          </div>
         </div>
       </div>
     </>
